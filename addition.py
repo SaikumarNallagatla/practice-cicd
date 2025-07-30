b65a9c4d369e45bc81623d8482db17cbb2454e32
saikumar <saikumarnallagatla@gmail.com>
2025-07-30 10:10:09 +0530

    gitflows

--- addition.py
+++ addition.py
@@ -1,8 +1,6 @@
-# app.py
-# This is a test commit
 def add(a, b):
     return a + b
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+if __name__ == "__main__":
+    result = add(2, 3)
+    print("The sum is:", result)
