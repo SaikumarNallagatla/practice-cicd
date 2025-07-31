864c3acf92f4351db06fb83ab99a27cce6e43e7c
saikumar <saikumarnallagatla@gmail.com>
2025-07-31 09:03:19 +0530

    s3 bucket

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def greet():
-    return "Hello from DevOps CI/CD Pipeline!"
+def lambda_handler(event=None, context=None):
+    return {
+        'statusCode': 200,
+        'body': 'Hello from DevOps Baby!'
+    }
+
+if __name__ == "__main__":
+    print("Deploy to S3 test successful!")
